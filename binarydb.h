f162601959ae1c846144bd1cbb21340883654073
gotospeleo99-coder <gotospeleo99@gmail.com>
2026-04-21 21:35:40 +0900

    binarydb.cpp周りの変更多数
    binarydb.hのクラス変更（再度変更予定）
    構造体の追加でヘッダー情報をその構造体に収納
    parseOHLCをdoubleに変更

--- binarydb.h
+++ binarydb.h
@@ -14,14 +14,18 @@ namespace binaryDB {
 int ExportTickerBinary();
 int firstExportPricedataBinary();
 };
-std::string GetFileDate();
+
 
 
 class file{
 public:
     static bool check();
     static std::vector<std::string> CSVparse(const std::string& line);
-    static std::optional<OHLCetc> parseOHLC(const std::vector<std::string>& fields);
+    static void parseOHLC(const std::vector<std::string>& fields,const CSVpriceheader& header, OHLCetc& data,int& date);
+    static std::string GetFileDate();
+    static void StockCodeCheck(std::vector<StockCodeID>& stockCode,std::filesystem::path path);//対象コードにIDを埋め込む
+    static std::vector<std::string>ListCSVparse(const std::string& line);
+    static bool InsertPriceRecord(std::filesystem::path p,OHLCetc data, CSVpriceheader hedear,int );
     template<typename T>
     bool Savebinary(const std::filesystem::path& path,const std::string& filename,const std::vector<T>* data);
     template<typename T>
@@ -40,11 +44,5 @@ public:
     }
 };
 
-class List_stocks{//バイナリー変換前工程用クラス
-public:
-    static void StockCodeCheck(std::vector<StockCodeID>& stockCode,std::filesystem::path path);//対象コードにIDを埋め込む
-    void ListLorder();
-    static std::vector<std::string>ListCSVparse(const std::string& line);
-};
 
 #endif // BINARYDB_H
--- struct_util.h
+++ struct_util.h
@@ -1,20 +1,26 @@
 #ifndef STRUCT_UTIL_H
 #define STRUCT_UTIL_H
 #include <cstdint>
-#include <string>
 
 // ---------------------------------------------------------------------
 // バイナリ用構造体
 // ---------------------------------------------------------------------
-struct OHLCetc{
-    int32_t  Open,High,Low,Close,UL,LL;
-    uint32_t Vo;
-    double AF;//AdjFactor
+struct OHLCetc{//データは10倍して保存,表示の際などに注意
+    int32_t  Open,High,Low,Close,UL,LL;//ULLLはストップ二種
+    uint32_t Vo;//出来高
+    float AF;//AdjFactor、株式分割合併
+
+    static constexpr int PRICE_SCALE = 10;
 }__attribute__((packed));
 struct StockCodeID{
     char code[6];
     uint16_t id;
 }__attribute__((packed));
+struct CSVpriceheader {
+    short Open=-1, High=-1, Low=-1, Close=-1;
+    short UL=-1, LL=-1, Vo=-1, AF=-1;
 
+    static constexpr size_t count = 8;
+};
 #endif // STRUCT_UTIL_H
 
